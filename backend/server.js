b11146fda6da9aa32a3fc9f35da32861c59fd590
sohanakhatun <sohanakhatun16@gmail.com>
2025-11-08 16:51:00 +0530

    added auth using json-server and json-server-auth , frontend - profile dashboard

--- backend/server.js
+++ backend/server.js
@@ -26,3 +26,4 @@ const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`JSON Server with Auth is running on http://localhost:${PORT}`);
 });
+
---
+++ frontend/src/components/ui/error-toast.jsx
@@ -0,0 +1,25 @@
+import { toast } from "sonner";
+
+import { Button } from "@/components/ui/button";
+
+const SoftDestructiveSonnerDemo = () => {
+  return (
+    <Button
+      variant="outline"
+      onClick={() =>
+        toast.error("Oops, there was an error processing your request.", {
+          style: {
+            "--normal-bg":
+              "color-mix(in oklab, var(--destructive) 10%, var(--background))",
+            "--normal-text": "var(--destructive)",
+            "--normal-border": "var(--destructive)",
+          },
+        })
+      }
+    >
+      Soft Destructive Toast
+    </Button>
+  );
+};
+
+export default SoftDestructiveSonnerDemo;
